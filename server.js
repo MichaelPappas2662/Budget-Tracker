e34c10c4a25ce0d02de213216f8698ec1811d944
Michael Pappas <mixalis.pappas23@gmail.com>
2021-07-13 19:59:08 +1000

    file: updating mongodb

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 });
 
 // routes
